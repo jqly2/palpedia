ef9bcff16201c9f0f28502b0ff151ed2e1fa7b13
Jonathan Ly <jonathanly213@gmail.com>
2024-03-25 15:56:28 -0700

    changed layout with more grid usage and offset dual elemental pals symbol.

--- src/app/deck/components/client/palCard.tsx
+++ src/app/deck/components/client/palCard.tsx
@@ -39,14 +39,14 @@ function checkDisplay(
 			<Image
 				src={ELEICONS[value]}
 				alt={value}
-				height={32}
-				width={32}
+				height={48}
+				width={48}
 			/>
 		);
 	} else if (query.includes('WorkSuitability_') && typeof value === 'number') {
 		return (
-			<>
-				<div className='relative h-6 w-6'>
+			<div className='grid grid-cols-2'>
+				<div className='relative h-7 w-7'>
 					<Image
 						src={WORKICONS[query]}
 						alt={query}
@@ -54,7 +54,7 @@ function checkDisplay(
 					/>
 				</div>
 				<p className='font-bold text-l mb-2 text-slate-100'>{value}</p>
-			</>
+			</div>
 
 		);
 	} else {
@@ -86,62 +86,72 @@ export default function PalCardComponent({
 	WorkSuitability_Watering,
 }: PalCardProps) {
 	return (
-		<div className='w-full h-full rounded-lg bg-slate-900 overflow-hidden shadow-lg hover:border border-cyan-600'>
-			<div className='px-6 py-4'>
-				<div className='relative flex flex-row items-center justify-between mb-4'>
+		<div className='w-full h-full rounded-lg px-5 py-5 grid grid-flow-rows-dense gap-2 bg-slate-800 overflow-hidden shadow-lg transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-900 duration-300'>
+				<div className='relative h-12 flex flex-row items-center justify-between mb-4'>
 
 					<div className='flex flex-row relative mt-4 ml-2'>					
 						<div className='absolute -left-4 -top-4 text-xs'>No.{ZukanIndex}{ZukanIndexSuffix}</div>
 						<div className='font-bold text-xl mb-2'>{EnglishName}</div></div>
-					<div className="flex flex-row mt-4">
-
-						<Image
-							src={ELEICONS[ElementType1]}
-							alt={ElementType1}
-							height={64}
-							width={64}
-						/>
-						{checkDisplay('ElementType2', ElementType2)}
+					<div className="flex">
+						{ (typeof ElementType2 === "string") 
+							? 	<>
+									<div className="absolute bottom-2 right-3.5">
+										<Image
+												src={ELEICONS[ElementType1]}
+												alt={ElementType1}
+												height={48}
+												width={48}
+											/>
+									</div>
+									<div className="absolute top-3.5 -right-2">
+										{checkDisplay('ElementType2', ElementType2)}
+									</div>
+								</>
+							: 	<div className="">
+									<Image
+											src={ELEICONS[ElementType1]}
+											alt={ElementType1}
+											height={56}
+											width={56}
+										/>
+								</div>
+						}
 					</div>
 				</div>
-				<div className='relative'>
+				<div className='relative h-80 w-80'>
 					<Image
 						className='border-2 border-slate-100 rounded-lg'
 						src={`https://palpedia-media.s3.us-west-1.amazonaws.com/Normal/T_${Name}_icon_normal.png`}
 						alt={Name}
-						height={256}
-						width={256}
+						fill={true}
 					/>
 				</div>
-				<div className="h-24">
-					<div className='flex flex-row'>
-						<div id="palcard-work-component" className="grid grid-cols-6 justify-end">
-							{checkDisplay('WorkSuitability_EmitFlame', WorkSuitability_EmitFlame)}
-							{checkDisplay('WorkSuitability_Collection', WorkSuitability_Collection)}
-							{checkDisplay('WorkSuitability_Cool', WorkSuitability_Cool)}
-							{checkDisplay('WorkSuitability_Deforest', WorkSuitability_Deforest)}
-							{checkDisplay('WorkSuitability_GenerateElectricity', WorkSuitability_GenerateElectricity)}
-							{checkDisplay('WorkSuitability_Handcraft', WorkSuitability_Handcraft)}
-							{checkDisplay('WorkSuitability_Mining', WorkSuitability_Mining)}
-							{checkDisplay('WorkSuitability_MonsterFarm', WorkSuitability_MonsterFarm)}
-							{checkDisplay('WorkSuitability_ProductMedicine', WorkSuitability_ProductMedicine)}
-							{checkDisplay('WorkSuitability_Seeding', WorkSuitability_Seeding)}
-							{checkDisplay('WorkSuitability_Transport', WorkSuitability_Transport)}
-							{checkDisplay('WorkSuitability_Watering', WorkSuitability_Watering)}
+				<div className="h-auto w-auto grid grid-cols-2 grid-flow-col-dense mt-5">
+					<div id="palcard-work-component" className="grid grid-cols-2 grid-flow-row border-2 border-slate-400 rounded-lg">
+						{checkDisplay('WorkSuitability_EmitFlame', WorkSuitability_EmitFlame)}
+						{checkDisplay('WorkSuitability_Collection', WorkSuitability_Collection)}
+						{checkDisplay('WorkSuitability_Cool', WorkSuitability_Cool)}
+						{checkDisplay('WorkSuitability_Deforest', WorkSuitability_Deforest)}
+						{checkDisplay('WorkSuitability_GenerateElectricity', WorkSuitability_GenerateElectricity)}
+						{checkDisplay('WorkSuitability_Handcraft', WorkSuitability_Handcraft)}
+						{checkDisplay('WorkSuitability_Mining', WorkSuitability_Mining)}
+						{checkDisplay('WorkSuitability_MonsterFarm', WorkSuitability_MonsterFarm)}
+						{checkDisplay('WorkSuitability_ProductMedicine', WorkSuitability_ProductMedicine)}
+						{checkDisplay('WorkSuitability_Seeding', WorkSuitability_Seeding)}
+						{checkDisplay('WorkSuitability_Transport', WorkSuitability_Transport)}
+						{checkDisplay('WorkSuitability_Watering', WorkSuitability_Watering)}
+					</div>
+
+					<div id="palcard-stat-component" className="grid grid-rows-2 items-center gap-3">
+						<div className='flex flex-row justify-center'>
+							<p className="font-bold text-base bg-red-700">HP: {HP}</p>
+						</div>
+						<div className='flex flex-row justify-end gap-4'>
+							<p className="font-bold text-1 mb-2 bg-orange-500">HP: {ShotAttack}</p>
+							<p className="font-bold text-1 mb-2 bg-blue-600">DEF: {Defense}</p>
 						</div>
 					</div>
 				</div>
-
-				<div id="palcard-stat-component" className="flex flex-row justify-between">
-					<div>
-						<p className="font-bold text-base bg-red-700 mt-4 mb-4">HP: {HP}</p>
-					</div>
-					<div className='flex flex-row justify-end mt-4 mb-4'>
-						<p className="font-bold text-1 mb-2 bg-orange-500">HP: {ShotAttack}</p>
-						<p className="font-bold text-1 mb-2 bg-blue-600">DEF: {Defense}</p>
-					</div>
-				</div>
-			</div>
 		</div>
 	);
 }
